81eb091df1004ec2516229bcb452ae6dd21452de
Saviour Philip <ukosaviour21@gmail.com>
2024-06-06 02:06:49 +0100

    otp expiration time increase

--- src/otp/schemas/otp.schema.ts
+++ src/otp/schemas/otp.schema.ts
@@ -5,7 +5,7 @@ import { OtpEnumType } from '../enum/otp.enum';
 
 export type otpDocument = Otp & Document;
 
-@Schema({ expireAfterSeconds: 3600 })
+@Schema()
 export class Otp {
   @Field()
   @Prop({ type: String, required: true })
@@ -19,13 +19,15 @@ export class Otp {
   @Prop({ type: Number, required: true })
   code: number;
 
-  @Field()
-  @Prop({ type: Date, default: new Date() })
+  @Prop({ type: Date, default: Date.now })
   createdAt: Date;
 
-  @Field()
-  @Prop({ type: Date, default: Date.now(), expireAfterSeconds: 3600 })
-  expires: Date;
+  @Prop({
+    type: Date,
+    default: () => new Date(Date.now() + 30 * 60 * 1000),
+    expires: '30m',
+  })
+  expiresAt: Date;
 }
 
 export const OtpSchema = SchemaFactory.createForClass(Otp);
